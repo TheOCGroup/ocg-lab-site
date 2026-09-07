0f2e93667c86bc8aef68bae4dda77b71d22708f7
TheOCGroup <gocasiorealtor@gmail.com>
2026-09-07 11:15:19 -0500

    Expose REI QA lock

---
+++ playbooks/real-estate-investor/qa-lock.js
@@ -0,0 +1,7 @@
+const REI_QA = {
+  coverAsset: '/playbooks/real-estate-investor/rei-approved-book.webp',
+  master: 'Insurance Agent Digital Product',
+  cover: 'Approved Real Estate Investor retail book',
+  release: 'etsy-candidate'
+};
+window.REI_QA = REI_QA;
